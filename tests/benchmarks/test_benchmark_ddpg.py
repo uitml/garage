2d290d5ffb47ac223fe7a169cb2edd3bfcf53e96
Peter Lillian <plillian@usc.edu>
2019-02-27 12:48:21 -0800

    Imports

--- tests/benchmarks/test_benchmark_ddpg.py
+++ tests/benchmarks/test_benchmark_ddpg.py
@@ -28,8 +28,8 @@ import pandas as pd
 import tensorflow as tf
 
 from garage.experiment import LocalRunner
-from garage.logger import CsvOutput, logger as garage_logger, TensorBoardOutput
 from garage.exploration_strategies import OUStrategy
+from garage.logger import CsvOutput, logger as garage_logger, TensorBoardOutput
 from garage.misc import ext
 from garage.replay_buffer import SimpleReplayBuffer
 from garage.tf.algos import DDPG
