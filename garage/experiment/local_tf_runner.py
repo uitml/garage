9c1d95697180c549e677f613b72dc850cb8a751e
Anson Wong <ahtsans@gmail.com>
2019-03-12 20:27:02 -0700

    Fixing DQN

--- garage/experiment/local_tf_runner.py
+++ garage/experiment/local_tf_runner.py
@@ -9,8 +9,8 @@ import time
 import tensorflow as tf
 
 from garage.misc.logger import logger
-from garage.sampler.parallel_sampler import singleton_pool
-from garage.tf.algos import BatchPolopt
+from garage.sampler.stateful_pool import singleton_pool
+from garage.tf.algos.batch_polopt import BatchPolopt
 from garage.tf.plotter import Plotter
 from garage.tf.samplers import BatchSampler
 from garage.tf.samplers import OffPolicyVectorizedSampler
