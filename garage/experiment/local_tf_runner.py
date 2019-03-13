4b4595bef8448f0da8d74378d30acee9c876690d
Keren Zhu <naeioi@hotmail.com>
2019-03-12 20:15:59 -0700

    Save env in runner
    This commit saves env in runner because algo not longer
    has access to the actual env. A test is also added to
    test the presence and integrity of both env and policy
    in snapshot.
    
    Signed-off-by: Keren Zhu <naeioi@hotmail.com>

--- garage/experiment/local_tf_runner.py
+++ garage/experiment/local_tf_runner.py
@@ -9,7 +9,7 @@ import time
 import tensorflow as tf
 
 from garage.misc.logger import logger
-from garage.sampler.parallel_sampler import singleton_pool
+from garage.sampler.stateful_pool import singleton_pool
 from garage.tf.algos import BatchPolopt
 from garage.tf.plotter import Plotter
 from garage.tf.samplers import BatchSampler
@@ -127,7 +127,7 @@ class LocalRunner:
 
     def initialize_tf_vars(self):
         """Initialize all uninitialized variables in session."""
-        with tf.name_scope("auto-initialized"):
+        with tf.name_scope("initialize_tf_vars"):
             self.sess.run(
                 tf.variables_initializer([
                     v for v in tf.global_variables()
@@ -172,8 +172,11 @@ class LocalRunner:
             paths: Batch of samples after preprocessed.
 
         """
+        assert self.has_setup
+
         logger.log("Saving snapshot...")
         params = self.algo.get_itr_snapshot(itr)
+        params['env'] = self.env
         if paths:
             params["paths"] = paths
         logger.save_itr_params(itr, params)
